157833486b0ab49c58d21f46c26081da97eac123
Regina Salikhova <yanadera1873@gmail.com>
2025-09-18 19:04:03 +0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+[![AHJ](https://github.com/Regina73G/ahj_form_popovers/actions/workflows/web.yml/badge.svg)](https://github.com/Regina73G/ahj_testing/actions/workflows/web.yml)
+
+[Github Pages](https://github.com/Regina73G/ahj_form_popovers)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  devtool: 'source-map',
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'img/[name][ext][query]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        title: "Document",
+        template: "./src/index.html",
+        favicon: path.resolve(__dirname, 'src/img/favicon.ico'),
+      }),
+      new MiniCSSExtractPlugin(),
+    ]
+  }
